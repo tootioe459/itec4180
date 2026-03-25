f227b32783ec4235f0d0ea0af94f663120a8b2cc
EZJae <yujinkim.business@gmail.com>
2026-03-25 15:08:43 -0400

    Create logout.php

---
+++ public/logout.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/src/bootstrap.php';
+
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params['path'],
+        $params['domain'],
+        $params['secure'],
+        $params['httponly']
+    );
+}
+
+session_destroy();
+
+Response::redirect('index.php');
