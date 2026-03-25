dfef1779d3cdfa9ad5af6ea0f5ccd0bf3ab3da39
EZJae <yujinkim.business@gmail.com>
2026-03-25 15:03:20 -0400

    Create env.example.php

---
+++ env.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'APP_NAME' => 'Simple PHP Chat',
+    'DB_HOST' => 'localhost',
+    'DB_PORT' => '3306',
+    'DB_NAME' => 'simple_chat',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+];
